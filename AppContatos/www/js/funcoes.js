4d56786ce5dab516a3f778037e6762136c735347
SantiagoVinicius <viniciussantiagosantos19@gmail.com>
2022-07-20 20:55:35 -0300

    .

--- AppContatos/www/js/funcoes.js
+++ AppContatos/www/js/funcoes.js
@@ -1,13 +1,23 @@
 function SalvarAnotacao(){
     novoContato = document.getElementById("nome").value
     novoTel = document.getElementById("tel").value
-    document.getElementById("contatos-salvos").innerHTML += "<p>" +
+    document.getElementById("contatosSalvos").innerHTML += "<p>" +
     "Nome: " + novoContato + " " + "Telefone: " + novoTel + "</p>"
     document.getElementById("nome").value = " " 
     document.getElementById("tel").value = " " 
   
-
+    localStorage.contatosSalvos = document.getElementById("contatosSalvos").innerHTML
 }
+function carregar(){
+    if(localStorage.contatosSalvos){
+        document.getElementById("contatosSalvos").innerHTML = 
+        localStorage.contatosSalvos
+    }
+}
+
+window.addEventListener("load", function(){
+    carregar()
+})
 
 document.getElementById("botao-salvar").addEventListener("click", function(){
     SalvarAnotacao()
